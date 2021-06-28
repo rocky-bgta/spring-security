317a9244caf9b995de6944f47a3f21c2540a19f9
Salahin <salahin.dsi@bkash.com>
2021-06-28 15:19:08 +0600

    start adding db

--- src/main/java/com/salahin/springsecurity/configuration/CustomUserDetailsService.java
+++ src/main/java/com/salahin/springsecurity/configuration/CustomUserDetailsService.java
@@ -1,5 +1,9 @@
 package com.salahin.springsecurity.configuration;
 
+import com.salahin.springsecurity.entity.RoleEntity;
+import com.salahin.springsecurity.entity.UserEntity;
+import com.salahin.springsecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,24 +11,35 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 	
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		List<SimpleGrantedAuthority> roles = null;
-		if(username.equals("admin")){
-			roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
-			return new User("admin","$2a$10$hZ6d9g1WRNqvHXtySt.1yO4L.PklA.fzodiTjv/4jHhlks4vSrhi2", roles);
+		List<SimpleGrantedAuthority> roles = new ArrayList<>();
+		UserEntity userEntity;
+		userEntity = userRepository.findByUsername(username);
+		if(userEntity != null){
+			for (RoleEntity roleEntity : userEntity.getRoleList()) {
+				roles.add(new SimpleGrantedAuthority(roleEntity.getRoleName()));
+			}
+			return new User(userEntity.getUsername(),userEntity.getPassword(), roles);
 		}
-		
-		if(username.equals("user")){
-			roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-			return new User("user","$2a$10$/iW5mfE.3iXS9vvRt7Etfe9pnObLtfRtJiiGhBML.G3yQZhRRmRhy", roles);
-		}
+//		if(username.equals("admin")){
+//			roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+//			return new User("admin","$2a$10$hZ6d9g1WRNqvHXtySt.1yO4L.PklA.fzodiTjv/4jHhlks4vSrhi2", roles);
+//		}
+//
+//		if(username.equals("user")){
+//			roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+//			return new User("user","$2a$10$/iW5mfE.3iXS9vvRt7Etfe9pnObLtfRtJiiGhBML.G3yQZhRRmRhy", roles);
+//		}
 		
 		throw new UsernameNotFoundException("User not found with the name"+ username);
 	}
